51dac145a02237aaf291b932642de1b10bd8ab4f
Blackcode2 <doky03115@gmail.com>
2024-12-16 13:46:21 +0900

    Initial Commit

---
+++ AESPArd_FE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AESPArd_FE
+//
+//  Created by KimDogyung on 12/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AESPArd_FETests/AESPArd_FETests.swift
@@ -0,0 +1,17 @@
+//
+//  AESPArd_FETests.swift
+//  AESPArd_FETests
+//
+//  Created by KimDogyung on 12/16/24.
+//
+
+import Testing
+@testable import AESPArd_FE
+
+struct AESPArd_FETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
