572fa44e70dd3d0bbbb06dbbc931f5fd5ec3a6b2
blackcode2 <doky03115@gmail.com>
2024-12-26 13:36:37 +0900

    [Feat]: URLClass 생성

---
+++ AESPArd_FE/Network/URLClass.swift
@@ -0,0 +1,19 @@
+//
+//  URLClass.swift
+//  AESPArd_FE
+//
+//  Created by KimDogyung on 12/26/24.
+//
+
+//
+//  URLClass.swift
+//  AESPArd_FE
+//
+//  Created by 이유현 on 12/26/24.
+//
+
+import UIKit
+
+class URLClass {
+    var baseURL : String = "#"
+}
